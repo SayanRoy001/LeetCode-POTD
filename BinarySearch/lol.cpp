6115445aac6ec96707f9aeac4a9310f7dcbea4cd
Sayan Roy <sayanroy7987@gmail.com>
2025-03-15 21:37:23 +0530

    Settings

---
+++ BinarySearch/lol.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // Your code here
+    vector<int>num;
+    num.push_back(1);
+    cout<<num[0];
+    return 0;
+}
