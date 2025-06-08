012616d43839700fe8a77eb221507009d92a6655
Sayan Roy <sayanroy7987@gmail.com>
2025-06-08 17:45:01 +0530

    Should do by dfs try

---
+++ JUNE/Graphs/LC_386.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+//TC-o(n(logn)^2)
+    vector<int> lexicalOrder(int n) {
+        vector<in>ans;
+        priority_queue<string,vector<string>,greater<string>>pq;
+        for(int i=1;i<=n;i++){
+            pq.push(to_string(i));
+        }
+        while(!pq.empty()){
+            ans.push_back(stoi(pq.top()));
+            pq.pop();
+        }
+        return ans;
+    }
+};
