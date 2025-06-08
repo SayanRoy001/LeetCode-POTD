ab983aaa6d7c1c2a9a9dac00b25c55615746acc3
Sayan Roy <sayanroy7987@gmail.com>
2025-06-08 17:45:26 +0530

    DFS

--- JUNE/Graphs/LC_386.cpp
+++ JUNE/Graphs/LC_386.cpp
@@ -6,7 +6,7 @@ class Solution {
 public:
 //TC-o(n(logn)^2)
     vector<int> lexicalOrder(int n) {
-        vector<in>ans;
+        vector<int>ans;
         priority_queue<string,vector<string>,greater<string>>pq;
         for(int i=1;i<=n;i++){
             pq.push(to_string(i));
